84599a98edfcb0da18fccd722e7d577d53476952
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-11 23:42:15 +0530

    frontend userapp view

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Addlogin from './components/Addlogin';
 import Signup from './components/Signup';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
     <div>
       <Addlogin/>
       <Signup/>
+      <Viewall/>
     </div>
   );
 }
--- src/components/Viewall.jsx
+++ src/components/Viewall.jsx
@@ -12,24 +12,33 @@ const Viewall = () => {
       
       <th scope="col">name</th>
       <th scope="col">dob</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Handle</th>
+      <th scope="col">bloodgrp</th>
+      <th scope="col">mobileno</th>
+      <th scope="col">address</th>
+      <th scope="col">pincode</th>
+      <th scope="col">district</th>
+      <th scope="col">place</th>
+      <th scope="col">emailid</th>
+      <th scope="col">username</th>
+      <th scope="col">password</th>
+      <th scope="col">confirmpassword</th>
     </tr>
   </thead>
   <tbody>
     <tr>
      
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    <th scope="col">name</th>
+      <td scope="col">dob</td>
+      <td scope="col">bloodgrp</td>
+      <td scope="col">mobileno</td>
+      <td scope="col">address</td>
+      <td scope="col">pincode</td>
+      <td scope="col">district</td>
+      <td scope="col">place</td>
+      <td scope="col">emailid</td>
+      <td scope="col">username</td>
+      <td scope="col">password</td>
+      <td scope="col">confirmpassword</td>
     </tr>
     <tr>
       <th scope="row">2</th>
